7a073db6e9ed065cea57077bd0f37b5e6118676d
kintsoogi <kintsoogii@proton.me>
2023-08-24 13:01:20 -0500

    v3.4.5-beta.1

---
+++ src/components/ScriptureCard/ScriptureCard.spec.tsx
@@ -0,0 +1,28 @@
+
+import { getCurrentBook } from "./ScriptureCard";
+
+describe('getCurrentBook', () => {
+  it('should succeed when blah blah', () => {
+      // Given
+      const bookId = 'tit'
+      const filename = '57-TIT.usfm'
+      const scriptureConfig = {
+        bookObjects: {
+          chapters: {
+            1: {
+              1: 'stuff'
+            },
+          },
+          headers: [],
+          name: filename
+        }
+      }
+
+      // when
+      const bibleUsfm = getCurrentBook(scriptureConfig, bookId)
+
+      // then
+      console.log(bibleUsfm)
+      expect(bibleUsfm).toBeTruthy()
+  })
+})
